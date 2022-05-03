d6dd240c628fd6126ef314e80cb0ebb53aa40163
EnzOuille <enzo_cisternino@hotmail.fr>
2022-05-03 10:38:17 +0200

    Work In Progress : Traitement des commandes

--- src/main/java/gl/application/Application.java
+++ src/main/java/gl/application/Application.java
@@ -3,9 +3,13 @@ package gl.application;
 import gl.ihm.Choices;
 import gl.ihm.ChoicesAbstract;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Application {
+
+    private Logger logger = Logger.getLogger(Application.class.getName());
 
     public final static int STATE_ANONYMOUS = 0;
     public final static int STATE_CONNECTED = 1;
@@ -21,20 +25,27 @@ public class Application {
     }
 
     public void start() {
-        while (true) {
+        String asking = "Veuillez saisir le numéro de l'action que vous souhaitez effectuer.";
+        int line = 0;
+        Boolean condition = true;
+        while (condition) {
             switch(this.state){
                 case(Application.STATE_ANONYMOUS):
                     choices.display(STATE_ANONYMOUS);
+                    line = askForIntegerLine(asking);
+                    condition = parseOptions(line);
                     break;
                 case(Application.STATE_CONNECTED):
                     choices.display(STATE_CONNECTED);
+                    line = askForIntegerLine(asking);
+                    condition = parseOptions(line);
                     break;
                 case(Application.STATE_MANAGER):
                     choices.display(STATE_MANAGER);
+                    line = askForIntegerLine(asking);
+                    condition = parseOptions(line);
                     break;
             }
-            int line = askForIntegerLine("Veuillez saisir le numéro de l'action que vous souhaitez effectuer : \n_");
-            parseOptions(line);
         }
     }
 
@@ -45,18 +56,23 @@ public class Application {
 
     public int askForIntegerLine(String param) {
         System.out.println(param);
-        try {
-            return Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            return 0;
+        while(true){
+            try{
+                int res = Integer.parseInt(scanner.nextLine());
+                return res;
+            }catch(NumberFormatException e){
+                System.out.println("Veuillez saisir un entier !");
+            }
         }
     }
 
-    public void parseOptions(int line) {
+    public Boolean parseOptions(int line) {
         ChoicesAbstract choice = this.choices.getFunction(line, state);
         if (choice != null) {
             choice.execute(scanner);
+            return true;
         }
+        System.out.println("Aucune option trouvée, veuillez réésayez svp");
+        return true;
     }
 }
