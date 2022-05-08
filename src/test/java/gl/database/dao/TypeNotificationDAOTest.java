c32a38ce65be40706727d368fffd03690e206fae
ChineThug <francoisxu2000@gmail.com>
2022-05-08 11:47:10 +0200

    Fonctionnalité (TypeNotificationDAO.java) : Réalisation des tests pour les DAO - Création des tests sur TypeNotificationDAO

--- src/test/java/gl/database/dao/TypeNotificationDAOTest.java
+++ src/test/java/gl/database/dao/TypeNotificationDAOTest.java
@@ -1,2 +1,21 @@
-package gl.database.dao;public class TypeNotificationDAOTest {
+package gl.database.dao;
+
+import gl.database.model.TypeNotification;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TypeNotificationDAOTest {
+
+    @Test
+    public void testGetTypeNotification(){
+        List<TypeNotification> listOfTypeNotification = TypeNotificationDAO.getTypeNotification();
+        assertThat(listOfTypeNotification)
+                .extracting(TypeNotification::getId_typeNotification)
+                .contains(TypeNotification.TYPE_MAIL)
+                .contains(TypeNotification.TYPE_SMS)
+                .hasSize(2);
+    }
 }
