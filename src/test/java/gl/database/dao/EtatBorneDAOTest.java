f913743900f32fdf15fbb957cb40554b8d3ce76b
ChineThug <francoisxu2000@gmail.com>
2022-05-08 11:37:56 +0200

    Fonctionnalité (EtatBorne.java) : Réalisation des tests pour les DAO - Création des tests sur EtatBorneDAO

--- src/test/java/gl/database/dao/EtatBorneDAOTest.java
+++ src/test/java/gl/database/dao/EtatBorneDAOTest.java
@@ -1,2 +1,22 @@
-package gl.database.dao;public class EtatBorneDAOTest {
+package gl.database.dao;
+
+import gl.database.model.EtatBorne;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+public class EtatBorneDAOTest {
+
+    @Test
+    public void testGetEtatBorne(){
+        List<EtatBorne> listOfEtatBorne = EtatBorneDAO.getEtatBorne();
+        assertThat(listOfEtatBorne)
+                .extracting(EtatBorne::getId_etatBorne)
+                .contains(EtatBorne.STATE_AVAILABLE)
+                .contains(EtatBorne.STATE_UNAVAILABLE)
+                .contains(EtatBorne.STATE_RESERVED)
+                .contains(EtatBorne.STATE_BUSY)
+                .hasSize(4);
+    }
 }
