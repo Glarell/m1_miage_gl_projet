2f22898e27da31e0d6fe243a724c827e3d2acc4f
Tondon César <pilou.tondon@hotmail.fr>
2022-05-15 14:11:50 +0200

    Fonctionnalité (AddImmatChoice) : C2 Renseigner plaques d'immatriculation véhicules

--- src/main/java/gl/commands/client/AddImmatChoice.java
+++ src/main/java/gl/commands/client/AddImmatChoice.java
@@ -3,14 +3,28 @@ package gl.commands.client;
 import gl.application.Application;
 import gl.application.User;
 import gl.commands.ChoicesAbstract;
+import gl.database.dao.PlaqueDAO;
+import gl.database.model.Plaque;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AddImmatChoice extends ChoicesAbstract {
 
     @Override
     public int execute(Scanner scanner, User user) {
-        System.out.println("Ajouter une immatriculation");
+        System.out.println("[------Ajouter une immatriculation------]\n");
+        //[A-Z]{2}-\d{3}-[A-Z]{2}
+        String plaque_id = Application.askForLine("Saisir numéro d'immatriculation :");
+        while (!plaque_id.matches("[A-Z]{2}-\\d{3}-[A-Z]{2}")) {
+            plaque_id = Application.askForLine("Mauvaise saisie !\nSaisir numéro d'immatriculation :");
+        }
+        try {
+            PlaqueDAO.insertNewPlaque(plaque_id);
+            System.out.printf("La plaque d'immatriculation %s a été ajoutée !",plaque_id);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
         return Application.RETURN_SUCCESS;
     }
 }
