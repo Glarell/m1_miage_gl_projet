95900e8a0433172a4f471d9dc07e2330bfa05c15
ChineThug <francoisxu2000@gmail.com>
2022-05-23 06:21:28 +0200

    Fonctionnalité (ClientDAO) : Conclure contrat - Ajout de nouvelles méthodes de tests pour ClientDAO.java

--- src/test/java/gl/database/dao/ClientDAOTest.java
+++ src/test/java/gl/database/dao/ClientDAOTest.java
@@ -1,15 +1,21 @@
 package gl.database.dao;
 
+import gl.database.model.Abonnement;
 import gl.database.model.Client;
+import gl.database.model.EstAssocie;
+import gl.database.model.Reservation;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClientDAOTest {
-
     @Test
     public void testGetListClient() {
         ArrayList<Client> list = (ArrayList<Client>) ClientDAO.getListClient();
@@ -28,7 +34,7 @@ public class ClientDAOTest {
 
     @Test
     public void testGetClientByEmailPwd() {
-        Client client = ClientDAO.getClientByEmailPwd("Admin@gmail.com","root");
+        Client client = ClientDAO.getClientByEmailPwd("Admin@gmail.com", "root");
         assertThat(client.getEmail()).isEqualTo("Admin@gmail.com");
         assertThat(client.getAdresse()).isEqualTo("Admin");
         assertThat(client.getPrenom()).isEqualTo("Admin");
@@ -45,7 +51,7 @@ public class ClientDAOTest {
 
     @Test
     public void testRegistrerClientWithId() throws SQLException {
-        Client client = new Client(7,"","","","","","","");
+        Client client = new Client(7, "", "", "", "", "", "", "");
         ClientDAO.registrerClientWithId(client);
         client = ClientDAO.getClient(7);
         assertThat(client.getId_client()).isNotEqualTo(-1);
@@ -53,7 +59,7 @@ public class ClientDAOTest {
 
     @Test
     public void testRegistrerClient() throws SQLException {
-        Client client = new Client("","","","","test","","");
+        Client client = new Client("", "", "", "", "test", "", "");
         client = ClientDAO.registrerClient(client);
         assertThat(client.getEmail()).isEqualTo("test");
     }
@@ -62,5 +68,45 @@ public class ClientDAOTest {
     public void testIsEmailAlreadyExist() {
         assertThat(ClientDAO.isEmailAlreadyExist("test")).isTrue();
         assertThat(ClientDAO.isEmailAlreadyExist("dsqdeaeazea")).isFalse();
+    }
+
+    @Test
+    public void testGetNbReservationAndAbonnement() throws SQLException, ParseException {
+        Client client = new Client("test", "test", "test", "test", "test", "test", "test");
+        client.setId_client(999);
+        ClientDAO.registrerClientWithId(client);
+
+        PlaqueDAO.insertNewPlaque("plaque_test");
+        EstAssocieDAO.insertNewEstAssocie(new EstAssocie(999, "plaque_test"));
+
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        Date date = new Date(format.parse("01-01-2025 00:00").getTime());
+
+        Abonnement abonnement = new Abonnement(date,
+                new Time(format.parse("01-01-2025 10:00").getTime()),
+                new Time(format.parse("01-01-2025 10:30").getTime()),
+                999, 1);
+        AbonnementDAO.registrerAbonnement(abonnement);
+
+        Reservation reservation = new Reservation(999, date,
+                new Time(format.parse("01-01-2022 10:00").getTime()),
+                new Time(format.parse("01-01-2022 10:30").getTime()),
+                0, false, EstAssocieDAO.getEstAssocieByClient(999).get(0).getId_estAssocie(), 1);
+        ReservationDAO.registrerReservationWithId(reservation);
+
+        assertThat(ClientDAO.getNbReservationAndAbonnement(999))
+                .isEqualTo(2);
+
+        ReservationDAO.deleteOldReservation(999);
+        EstAssocieDAO.deleteOldEstAssocie(999, "plaque_test");
+        AbonnementDAO.deleteOldAbonnementByClient(999);
+        ClientDAO.deleteClient(999);
+        PlaqueDAO.deleteOldPlaque("plaque_test");
+    }
+
+    @Test
+    public void testGetNbReservationAndAbonnementFromUnknowClient() {
+        assertThat(ClientDAO.getNbReservationAndAbonnement(999))
+                .isEqualTo(0);
     }
 }
