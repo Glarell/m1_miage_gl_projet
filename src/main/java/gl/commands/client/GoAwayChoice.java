1110af00862ac65da23f4da0d7472759c70ab318
ChineThug <francoisxu2000@gmail.com>
2022-05-27 13:07:54 +0200

    Fonctionnalité (COMMANDS) : Partir de sa réservation - Construction du squelette de la commande

--- src/main/java/gl/commands/client/GoAwayChoice.java
+++ src/main/java/gl/commands/client/GoAwayChoice.java
@@ -3,7 +3,10 @@ package gl.commands.client;
 import gl.application.Application;
 import gl.application.User;
 import gl.commands.ChoicesAbstract;
+import gl.database.dao.ReservationDAO;
+import gl.database.model.Reservation;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class GoAwayChoice extends ChoicesAbstract {
@@ -12,6 +15,28 @@ public class GoAwayChoice extends ChoicesAbstract {
         System.out.println("[------Partir de sa réservation------]");
         int res = Application.RETURN_SUCCESS;
 
+        Reservation reservation = GoAwayChoice.getReservation();
+        GoAwayChoice.generateReservationCost(reservation);
+
         return res;
+    }
+
+    /**
+     * Récupération de la réservation selectionnée par l'utilisateur
+     *
+     * @return la réservation
+     */
+    public static Reservation getReservation() {
+        Reservation reservation = new Reservation();
+        try {
+            ReservationDAO.updateArriveeReservation(reservation);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ReservationDAO.getReservationById(reservation.getId_reservation());
+    }
+
+    public static void generateReservationCost(Reservation reservation) {
+
     }
 }
