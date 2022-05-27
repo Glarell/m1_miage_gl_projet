0badfb068bd0f9ce8646f9458e1b6e7e5b037bf1
Glarell <pilou.tondon@hotmail.fr>
2022-05-27 16:08:12 +0200

    Fonctionnalité (DonnerReleveMensuel) : S2 - Envoyer une fois par mois le relevé mensuel
    Création classe pour génération du relevé

---
+++ src/main/java/gl/commands/client/DonnerReleveMensuel.java
@@ -0,0 +1,34 @@
+package gl.commands.client;
+
+import gl.application.Application;
+import gl.application.User;
+import gl.commands.ChoicesAbstract;
+import gl.database.dao.NotificationDAO;
+import gl.database.dao.ReservationDAO;
+import gl.database.model.Notification;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DonnerReleveMensuel extends ChoicesAbstract {
+    @Override
+    public int execute(Scanner scanner, User user) {
+        System.out.println("[------Affichage du relevé mensuel------]");
+        try {
+            double[] releve = ReservationDAO.getReleveMensuel();
+            LocalDate date = LocalDate.now();
+            String content = String.format("\t[Relevé du mois : %s]\n" +
+                    "\tLe nombre de réservations au cours de ce mois est de : %s\n" +
+                    "\tMontant total à payer : %s\n" +
+                    "[\\------------------------/]\n",date.getMonth(),releve[1],releve[0]);
+            System.out.println(content);
+            Notification notification = new Notification(content, Application.getCurrentClientId(), "Mail");
+            NotificationDAO.insertNewNotification(notification);
+            return Application.RETURN_SUCCESS;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Application.RETURN_FAILED;
+    }
+}
