ce70f858d6151aebae07257a0133272097fce663
ChineThug <francoisxu2000@gmail.com>
2022-05-27 23:21:27 +0200

    Fonctionnalité (ShowUpChoice.java) : Réalisation tests - Réalisation des tests sur les méthodes de la classe ShowUpChoice.java

---
+++ src/test/java/gl/commands/client/ShowUpChoiceTest.java
@@ -0,0 +1,74 @@
+package gl.commands.client;
+
+import gl.application.Application;
+import gl.application.User;
+import gl.database.dao.AbonnementDAO;
+import gl.database.dao.ClientDAO;
+import gl.database.dao.ReservationDAO;
+import gl.database.model.Abonnement;
+import gl.database.model.Reservation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+
+public class ShowUpChoiceTest {
+
+    private MockedStatic<Application> applicationMock;
+    private MockedStatic<ReservationDAO> reservationDAOMock;
+    private MockedStatic<AbonnementDAO> abonnementDAOMock;
+    private MockedStatic<ShowUpChoice> showUpChoiceMock;
+
+    @BeforeEach
+    public void init() {
+        applicationMock = mockStatic(Application.class);
+        reservationDAOMock = mockStatic(ReservationDAO.class);
+        abonnementDAOMock = mockStatic(AbonnementDAO.class);
+        showUpChoiceMock = mockStatic(ShowUpChoice.class);
+    }
+
+    @Test
+    public void testShowUpChoiceWithExistingReservation() {
+        reservationDAOMock.when(() -> ReservationDAO.getReservationFromCurrentDate(anyInt())).thenReturn(new Reservation(10, null, null, null, 0, false, -1, -1));
+        showUpChoiceMock.when(() -> ShowUpChoice.manageExistingReservation(Application.RETURN_SUCCESS)).thenReturn(Application.RETURN_SUCCESS);
+
+        ShowUpChoice showUpChoice = new ShowUpChoice();
+        assertThat(showUpChoice.execute(new Scanner(System.in), new User(Application.STATE_CONNECTED)))
+                .isEqualTo(Application.RETURN_SUCCESS);
+    }
+
+    @Test
+    public void testShowUpChoiceWithExistingAbonnement() {
+        reservationDAOMock.when(() -> ReservationDAO.getReservationFromCurrentDate(anyInt())).thenReturn(new Reservation());
+        abonnementDAOMock.when(() -> AbonnementDAO.getAbonnementFromCurrentDate(anyInt())).thenReturn(new Abonnement(10, null, null, null, -1, -1));
+        showUpChoiceMock.when(() -> ShowUpChoice.manageExistingAbonnement(Application.RETURN_SUCCESS)).thenReturn(Application.RETURN_SUCCESS);
+
+        ShowUpChoice showUpChoice = new ShowUpChoice();
+        assertThat(showUpChoice.execute(new Scanner(System.in), new User(Application.STATE_CONNECTED)))
+                .isEqualTo(Application.RETURN_SUCCESS);
+    }
+
+    @Test
+    public void testShowUpChoiceWithInexistingReservationAndAbonnement() {
+        reservationDAOMock.when(() -> ReservationDAO.getReservationFromCurrentDate(anyInt())).thenReturn(new Reservation());
+        abonnementDAOMock.when(() -> AbonnementDAO.getAbonnementFromCurrentDate(anyInt())).thenReturn(new Abonnement());
+
+        ShowUpChoice showUpChoice = new ShowUpChoice();
+        assertThat(showUpChoice.execute(new Scanner(System.in), new User(Application.STATE_CONNECTED)))
+                .isEqualTo(Application.RETURN_FAILED);
+    }
+
+    @AfterEach
+    public void end() {
+        applicationMock.close();
+        reservationDAOMock.close();
+        abonnementDAOMock.close();
+        showUpChoiceMock.close();
+    }
+}
